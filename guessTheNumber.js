398e4b23406f03c3b2273627da27685bd9e7ffb2
mihaelaiko <mihaelaikonomova@gmail.com>
2021-03-04 18:57:04 +0100

    randomnumberFunction

--- guessTheNumber.js
+++ guessTheNumber.js
@@ -1,6 +1,15 @@
-let randomNumber = Math.floor(Math.random() * (start - end + 1) + end);
+function generateRandomNumber(start, end){
+    start = Math.ceil(start);
+    end = Math.floor(end);
+    return Math.floor(Math.random() * (end - start+ 1) + start);
+  
+  }
 let rangesNumber = document.getElementById('ranges');
 let selectedRange = rangesNumber.value;
+if (selectedRange === 'level1'){
+    generateRandomNumber(1, 10);
+    
+}
 
 document.getElementById('submitGuess').onclick = function(){
     let userNumber = document.getElementById('guessField').value;
